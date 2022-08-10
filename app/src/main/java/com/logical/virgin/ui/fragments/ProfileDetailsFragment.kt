1bdd4ef60dff52088bbf20735f3652216fba2998
nadeemfouja294 <90181185+nadeemfouja294@users.noreply.github.com>
2022-08-10 14:09:45 +0100

    profile layout updated

--- app/src/main/java/com/logical/virgin/ui/fragments/ProfileDetailsFragment.kt
+++ app/src/main/java/com/logical/virgin/ui/fragments/ProfileDetailsFragment.kt
@@ -7,12 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.logical.virgin.databinding.FragmentProfileDetailsBinding
+import com.logical.virgin.databinding.ProfileLayoutBinding
 import com.logical.virgin.models.people.PeopleModelItem
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProfileDetailsFragment : Fragment() {
-    private var _binding: FragmentProfileDetailsBinding? = null
+    private var _binding:ProfileLayoutBinding? = null
     private val binding get() = _binding!!
 
 
@@ -21,7 +22,7 @@ class ProfileDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentProfileDetailsBinding.inflate(inflater, container, false)
+        _binding = ProfileLayoutBinding.inflate(inflater, container, false)
         val args = arguments
 
             val person: PeopleModelItem? = args?.getParcelable("information")
